d6497066ad46dd011c1a27ed628e72c7d311ff51
Emarilyne Wakio <emarilynew@gmail.com>
2024-03-30 18:19:46 +0300

    modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.getElementsByTagName("main")[0];
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Emarilyne is the champion";
